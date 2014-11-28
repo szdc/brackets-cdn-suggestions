9e33d59d58b1f8edffc3d2cdf29cfde02f4b3f1c
szdc <jack.willis-craig@hotmail.com>
2014-11-28 23:33:33 +1100

    Added regex to match the listed libraries.

--- main.js
+++ main.js
@@ -4,18 +4,23 @@
 define(function (require, exports, module) {
   'use strict';
   
-  var GOOGLE_HOSTED_LIBRARIES_URL = 'https://developers.google.com/speed/libraries/devguide';
+  var GOOGLE_HOSTED_LIBRARIES_URL = 'https://developers.google.com/speed/libraries/devguide',
+      REGEX_LIBRARIES = /div.*?>\s*?<dl>([\s\S]+?)<\/dl>/g;
   
   function downloadSource(url) {
+    function onload() {
+      parseLibraries(this.responseText);
+    }
+    
     var req = new XMLHttpRequest();
     req.onload = onload;
     req.open('get', url);
     req.send();
-    
-    function onload() {
-      return this.responseText();
-    }
   }
   
-  var source = downloadSource(GOOGLE_HOSTED_LIBRARIES_URL);
+  function parseLibraries(source) {
+    var libraryMatches = source.match(REGEX_LIBRARIES);
+  }
+  
+  downloadSource(GOOGLE_HOSTED_LIBRARIES_URL);
 });
