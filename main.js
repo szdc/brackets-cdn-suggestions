650bbf777b63f32d0614a0f63befde08823196a5
szdc <jack.willis-craig@hotmail.com>
2014-12-04 13:02:54 +1100

    Consolidated source and added library parsing.

--- main.js
+++ main.js
@@ -4,22 +4,41 @@
 define(function (require, exports, module) {
   'use strict';
   
-  var GOOGLE_HOSTED_LIBRARIES_URL = 'https://developers.google.com/speed/libraries/devguide',
-      REGEX_LIBRARIES = /div.*?>\s*?<dl>([\s\S]+?)<\/dl>/g;
+  // Modules
+  require('modules/DOMParser');
+  var Library = require('modules/Library');
   
+  // Constants
+  var GOOGLE_HOSTED_LIBRARIES_URL = 'https://developers.google.com/speed/libraries/devguide',
+      SELECTOR_LIBRARIES = 'div[itemprop=articleBody] > div[id]';
+  
+  // Fields
+  var libraries = [];
+  
+  /**
+   * Downloads and parses the Google Hosted Libraries webpage.
+   */
   function downloadSource(url) {
     function onload() {
-      parseLibraries(this.responseText);
+      var parser    = new DOMParser();
+      var document  = parser.parseFromString(this.responseText, 'text/html');
+      var libElements = document.body.querySelectorAll(SELECTOR_LIBRARIES);
+      
+      for (var i = 0; i < libElements.length; i++) {
+        var element = libElements[i],
+            library = Library.fromElement(element);
+      
+        if (library !== null) {
+          libraries.push(library);
+        }
+      }
     }
     
     var req = new XMLHttpRequest();
     req.onload = onload;
     req.open('get', url);
+    req.overrideMimeType('text/html; charset=x-user-defined');
     req.send();
-  }
-  
-  function parseLibraries(source) {
-    var libraryMatches = source.match(REGEX_LIBRARIES);
   }
   
   downloadSource(GOOGLE_HOSTED_LIBRARIES_URL);
