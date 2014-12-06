16f077213eb387692630fa97782232313cab0590
szdc <jack.willis-craig@hotmail.com>
2014-12-06 23:01:51 +1100

    Reorganised downloading and parsing.

--- main.js
+++ main.js
@@ -17,29 +17,35 @@ define(function (require, exports, module) {
   
   /**
    * Downloads and parses the Google Hosted Libraries webpage.
+   *
+   * @param {String}   The URL of the webpage to download
+   * @param {Function} A callback that handles the response
    */
-  function downloadSource(url) {
-    function onload() {
-      var parser    = new DOMParser();
-      var document  = parser.parseFromString(this.responseText, 'text/html');
-      var libElements = document.body.querySelectorAll(SELECTOR_LIBRARIES);
-      
-      for (var i = 0; i < libElements.length; i++) {
-        var element = libElements[i],
-            library = Library.fromElement(element);
-      
-        if (library !== null) {
-          libraries.push(library);
-        }
-      }
-    }
-    
+  function downloadSource(url, callback) {
     var req = new XMLHttpRequest();
-    req.onload = onload;
+    req.onload = callback;
     req.open('get', url);
     req.overrideMimeType('text/html; charset=x-user-defined');
     req.send();
   }
   
-  downloadSource(GOOGLE_HOSTED_LIBRARIES_URL);
+  function onDownloadComplete() {
+    var parser    = new DOMParser();
+    var document  = parser.parseFromString(this.responseText, 'text/html');
+    var libElements = document.body.querySelectorAll(SELECTOR_LIBRARIES);
+    parseLibraries(libElements);
+  }
+  
+  function parseLibraries(libElements) {
+    for (var i = 0; i < libElements.length; i++) {
+      var element = libElements[i],
+          library = Library.fromElement(element);
+
+      if (library !== null) {
+        libraries.push(library);
+      }
+    }
+  }
+  
+  downloadSource(GOOGLE_HOSTED_LIBRARIES_URL, onDownloadComplete);
 });
