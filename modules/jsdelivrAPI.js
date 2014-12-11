94d7ba3193ed7fa5f82a19dfed3b8c0bc6eeb19c
szdc <jack.willis-craig@hotmail.com>
2014-12-11 22:21:38 +1100

    Added jsdeliver API module.

---
+++ modules/jsdelivrAPI.js
@@ -0,0 +1,56 @@
+/*jslint vars: true */
+/*global define, $, brackets, window, console */
+
+define(function (require, exports, module) {
+  'use strict';
+  
+  var URL = 'http://api.jsdelivr.com/v1/{cdn}/libraries?fields=name,mainfile,versions',
+      CDN = Object.freeze({
+        BOOTSTRAP: 'bootstrap',
+        CDNJS:     'cdnjs',
+        GOOGLE:    'google',
+        JSDELIVR:  'jsdelivr',
+        JQUERY:    'jquery'
+      });
+  
+  /**
+   * Downloads the libraries hosted by the specified CDN.
+   *
+   * @param {String}
+   * The name of the CDN to download libraries from,
+   * as specified in the CDN variable.
+   *
+   * @param {Function} 
+   * A callback that handles the response.
+   */
+  function downloadLibraries(cdn, callback) {
+    var req = new XMLHttpRequest();
+    req.onload = callback;
+    req.open('get', URL.replace('{cdn}', cdn));
+    req.send();
+  }
+
+  /**
+   * Gets the libraries hosted by the specified CDN.
+   */
+  function getLibraries(cdn, callback) {
+    /**
+     * Checks the status code of the response before
+     * parsing the libaries.
+     */
+    function onDownloadComplete() {
+      if (this.status !== 200) {
+        callback('Error (Status ' + this.status + '): Invalid library?');
+        return;
+      }
+
+      var libraries = JSON.parse(this.responseText);
+      callback(null, libraries);
+    }
+    
+    downloadLibraries(cdn, onDownloadComplete);
+  }
+
+  exports.CDN = CDN;
+  exports.getLibraries = getLibraries;
+});
