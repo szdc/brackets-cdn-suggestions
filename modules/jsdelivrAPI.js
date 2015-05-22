8914e5d2aa8003ab27d5b2c8185f9a6a935043ce
David Humphrey (:humph) david.humphrey@senecacollege.ca <david.humphrey@senecacollege.ca>
2015-05-22 07:56:38 -0400

    Load jsDeliver JSON over HTTPS

--- modules/jsdelivrAPI.js
+++ modules/jsdelivrAPI.js
@@ -4,7 +4,7 @@
 define(function (require, exports, module) {
   'use strict';
   
-  var URL = 'http://api.jsdelivr.com/v1/{cdn}/libraries?fields=name,mainfile,versions';
+  var URL = 'https://api.jsdelivr.com/v1/{cdn}/libraries?fields=name,mainfile,versions';
   
   /**
    * Downloads the libraries hosted by the specified CDN.
